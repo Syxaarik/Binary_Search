30bf6d27ffc44d972eb8ff6ff632d962144df186
Artem <artemmarkovskiy2009@gmail.com>
2025-06-02 09:59:50 +0300

    Алгоритм быстрой сортировки

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-def binary_search(arr, item):
-    low = 0
-    high = len(arr) - 1
-
-    while high >= low:
-        mid = (low + high) // 2
-        guess = arr[mid]
-        if guess == item:
-            return mid
-        elif guess > item:
-            high = mid - 1
-        else:
-            low = mid + 1
-    return None
+def  findsmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
 
 
-my_list = [1, 3, 5, 7, 9]
+def selectionsort(arr):
+    newArr = []
+    copiedArr = list(arr)
+    for i in range(len(copiedArr)):
+        smallest = findsmallest(copiedArr)
+        newArr.append(copiedArr.pop(smallest))
+    return newArr
 
-print(binary_search(my_list, 3))
-print(binary_search(my_list, 5))
+
+print(selectionsort([10, 5, 2, 6,7, 3,2, 8,9, 7 ,1]))
